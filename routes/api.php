57893b2265eaf50037e79e63980263a736eca72e
mortezaa <mortezajafari76@gmail.com>
2025-09-27 13:13:38 +0330

    fix:route api middleware issue

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Mortezaa97\Stories\Http\Controllers\StoryController;
 use Mortezaa97\Support\Http\Controllers\DepartmentController;
 use Mortezaa97\Support\Http\Controllers\TicketController;
 
-Route::prefix('api/tickets')->group(function () {
+Route::prefix('api/tickets')->middleware('api')->group(function () {
     Route::post('/', [TicketController::class, 'store'])->middleware('auth:api')->name('tickets.store');
     Route::get('/', [TicketController::class, 'index'])->middleware('auth:api')->name('tickets.index');
     Route::get('/{ticket:code}', [TicketController::class, 'show'])->middleware('auth:api')->name('tickets.show');
